8d55782aaad7d7dc577686de8db4453d987b0ceb
LauZalan <laurinyeczzalan@gmail.com>
2026-04-20 18:17:38 +0200

    Spring backend project skeleton and Postgres db

---
+++ habit-tracker-be/src/test/java/io/zalan/habittrackerbe/HabitTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zalan.habittrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
